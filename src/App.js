5477e29760aa9b0bae4d22a76dd657576c71a310
Armen-MATEO <armmatosyan@gmail.com>
2020-11-07 20:07:22 +0400

    todo lists with tasks

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import BootstrapDemo from "./exercize/BootstrapDemo";
-//import { Market } from "./components/Food";
-//import { Weather } from "./components/Weather";
-//import Exchange from "./components/Exchange";
-//import { Product } from "./components/Product";
-import ToDo from "./components/ToDo";
-//import A from "./exercize/A";
+//import BootstrapDemo from "./exercize/BootstrapDemo";
+import ToDo from "./components/todo/ToDo";
+
 function App() {
   return (
     <div className="App">
       <ToDo />
-      <BootstrapDemo />
-      {/*
-      <A />
-      
-      <h2>super market</h2>
-      <Product
-        name="milk"
-        made="Armenia"
-        price="1$"
-        rate={490}
-        date="01.10.2020"
-        description="Eco"
-      />*/}
-
-      {/* <h3>Weather around the world</h3>
-      <Weather country="Armenia" city="Yerevan" day="+36 C" night="+15 C" />
-      <Weather country="Grenlandia" city="Nuuk" day="+5 C" night="-25 C" />
-      <h3>Super Market</h3>
-      <Market name="meet" price="3900 dram" />
-      <Market name="milk" price="400 dram" />
-      <h3>exchange</h3>
-      <Exchange currUSA="1$" currArm="490 dram" />*/}
     </div>
   );
 }
