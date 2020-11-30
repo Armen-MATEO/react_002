a801eaedbf7efd61169f4a11b4176c44269c7adf
Armen-MATEO <armmatosyan@gmail.com>
2020-11-30 20:08:33 +0400

    higher order functions

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 //import BootstrapDemo from "./exercize/BootstrapDemo";
 import ToDo from "./components/todo/ToDo";
+import AppDemo from "./exercize/AppDemo";
 //import A from "./exercize/AA";
 
 function App() {
   return (
     <div className="App">
       <ToDo />
+      <AppDemo />
     </div>
   );
 }
