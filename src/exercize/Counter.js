dd6910ec835fb78d4016c6798831a99ec1133ce5
Armen-MATEO <armmatosyan@gmail.com>
2020-12-16 20:17:27 +0400

    redux introduction

---
+++ src/exercize/Counter.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import ChangeCounter from "./ChangeCounter";
+import ShowCounter from "./ShowCounter";
+import Message from "./Message";
+import { connect } from "react-redux";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h2>{this.props.text}</h2>
+        <ShowCounter />
+        <ChangeCounter />
+        <Message />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    text: state.text,
+  };
+};
+
+export default connect(mapStateToProps, null)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,78 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+//import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import Counter from './exercize/Counter';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const defaultState = {
+  count: 0,
+  text: 'hello from Redux'
+};
+
+const reducer = (state = defaultState, action) => {
+  console.log('action', action)
+  console.log('state', state)
+
+switch(action.type){
+  case 'CHANGE_VALUE': {
+    return {
+      ...state,
+      count: state.count + action.value
+    };
+  }
+
+  case 'CHANGE_MESSAGE': {
+    return {
+      ...state,
+      text: action.message
+    }
+  }
+
+  default: return state;
+}
+
+  // if (action.type === 'CHANGE_VALUE') {
+  //   return {
+  //     ...state,
+  //     count: state.count + action.value
+  //   };
+
+  // }
+
+  // if(action.type === 'CHANGE_MESSAGE'){
+  //   return {
+  //     ...state,
+  //     text: action.message
+  //   }
+  // }
+
+  // return state;
+};
+
+const store = createStore(reducer);
+
 
 ReactDOM.render(
+  //<React.StrictMode>
+  //  <BrowserRouter>
+  //    <App />
+   // </BrowserRouter>
+  //</React.StrictMode>,
+
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Counter />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
+
+
+
   document.getElementById("root")
 );
 
